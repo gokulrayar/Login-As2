3771c56f8f81d3640818599a2d65a70a36aaf287
gokulrayar <gokulrayar>
2024-01-08 20:13:38 +0530

    register

---
+++ AuthLib/AuthLib/userlogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthLib
+{
+    public class RegisterLogin
+    {
+
+        private List<register> users;
+
+        public RegisterLogin()
+        {
+            // Initialize the list of users in the constructor
+            users = new List<register>();
+        }
+
+        public bool Register(string user, string pass)
+        {
+            // Check if the username already exists
+            if (users.Any(u => u.Username == user))
+            {
+                Console.WriteLine("Username already exists. Registration failed.");
+                return false;
+            }
+
+            // Create a new user and add it to the list
+            register newUser = new register() { Username = user, Password = pass };
+            users.Add(newUser);
+
+            Console.WriteLine("Registration successful!");
+            return true;
+        }
+
+        public bool Login(string username, string password)
+        {
+            // Check if the username and password match any user in the list
+            foreach (var user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    Console.WriteLine("Login Successful");
+                    return true;
+                }
+            }
+
+            Console.WriteLine("Login Failed");
+            return false;
+        }
+    }
+}
